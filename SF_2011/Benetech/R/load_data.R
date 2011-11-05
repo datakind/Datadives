1e12de943165f9f85bf9f15280d65c7a1954005b
Drew Conway <agconway@gmail.com>
2011-11-05 19:12:36 -0400

    Fixed pre-2002 dates to NA in date.uploaded

--- SF_2011/Benetech/R/load_data.R
+++ SF_2011/Benetech/R/load_data.R
@@ -12,6 +12,8 @@
 
 # Load libraries
 library(ggplot2)
+library(RJSONIO)
+library(RCurl)
 
 # Load data
 data.file <- '../data/martus-bullacct-4datadive-2011-11-03.csv'
@@ -20,6 +22,7 @@ benetech.full <- read.csv(data.file, stringsAsFactors=FALSE)
 # Format the dates
 benetech.full$Build.Date <- as.POSIXlt(benetech.full$Build.Date, format='%m/%d/%Y')
 benetech.full$date.uploaded <- as.POSIXlt(benetech.full$date.uploaded, format='%m/%d/%Y')
+benetech.full$date.uploaded[which(benetech.full$date.uploaded <= as.POSIXlt("2002-10-30"))] <- NA
 benetech.full$date.last.saved <- as.POSIXlt(benetech.full$date.last.saved, format='%m/%d/%Y')
 benetech.full$date.authorized <- as.POSIXlt(benetech.full$date.authorized, format='%m/%d/%Y')
 benetech.full$date.created <- as.POSIXlt(benetech.full$date.created, format='%m/%d/%Y')
@@ -43,6 +46,29 @@ benetech.full$version.number <- factor(benetech.full$version.number, ordered=TRU
 benetech.full$type <- factor(benetech.full$type, exclude='')
 benetech.full$group <- factor(benetech.full$group, exclude='')
 
+# Add geo coding
+
+# A function to query the Google Geo API. Get a lat/lon value for each address
+geocode.addr <- function(addr, sleep=TRUE) {
+    if(sleep) {
+        Sys.sleep(runif(1))
+    }
+    geo.url <- "http://maps.googleapis.com/maps/api/geocode/json?address="
+    geo.text <- getURL(paste(geo.url, URLencode(paste(addr, collapse="+")), "&sensor=false", sep=""))
+    geo.json <- fromJSON(geo.text)
+    if(geo.json$status == "OK"){
+        return(c(addr, geo.json$results[[1]]$geometry$location))
+    }
+    else{
+        if(geo.json$status == "OVER_QUERY_LIMIT") {
+                stop("Hit rate limit")
+        }
+        else {
+            return(c(addr, NA, NA))
+        }
+    }
+}
+
+
 # Subset to remove duplicates
 benetech <- subset(benetech.full, !test.bulletin & original.server & !tester)
-
