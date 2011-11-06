81c9f38261bcc65baa87c2cdcdd4cdcab3cab3a3
arokem <arokem@berkeley.edu>
2011-11-06 10:27:31 -0800

    ENH: Common words counts word frequencies in the titles scraped from the public website.

--- SF_2011/Benetech/python/common_words.py
+++ SF_2011/Benetech/python/common_words.py
@@ -1,23 +1,53 @@
+# To get the files, you need to run this in the "website" directory:
+# wget -l 1 -m -c "https://martus.ceu.hu/servlet/DoSearch?typeOfSearch=quickSearchAll"
+
 import numpy
+import re
+import csv
+import os
+import matplotlib.pyplot as plt
 
+out_file_name = "website_public_id.csv"
+out_file = file(out_file_name, "w")
+website_dir = "../website/martus.ceu.hu/servlet/"
+
+for file_name in os.listdir(website_dir):
+  if file_name.startswith("FoundBulletin"):
+    id = file_name.split("FoundBulletin?index=")[1].split("&")[0]
+    try:
+      body = file("%s%s"%(website_dir,file_name), "r").read()
+      title = body.split("<strong>")[1].split("</strong>")[0]
+      public_id = body.split("<!--Account Public Code = ")[1].split(" -->")[0]
+      bulletin_id = body.split("<!--Bulletin Local Id = ")[1].split(" -->")[0]
+      out_file.write("%s\t%s\t%s\n" % (title, public_id, bulletin_id))
+    except:
+      print "Cannot parse %s" % file_name
+
+out_file.close()
+
+# Now read it in and parse: 
+in_file = file(out_file_name, 'r')
 word_to_count = {}
-for line in body:
-  count_and_word = line.lstrip().rstrip().split(" ")
-  count = count_and_word[0]
-  if len(count_and_word) == 2:
-    word = count_and_word[1]
-  else:
-    word = ""
-  word_to_count[word] = count
+while 1:
+  line = in_file.readline()
+  if not line :
+    break
+  title = line.split("\t")[0].split(" ")
+  for word in title:
+    if word.lower() in word_to_count.keys():
+      word_to_count[word.lower()] += 1
+    else:
+      word_to_count[word.lower()] = 1
 
-word_to_count.values
-numpy.array(word_to_count.keys())[numpy.argsort(word_to_count.values())][-50:]
-# array(['formed', 'Thanpyuzayart', 'ya', 'fruit', '20', 'following',
-#        'Division', 'Time)', 'what', 'Operation', '2007', 'endure', 'been',
-#        'leader', 'most', 'demanded', 'participation', 'physical', 'arrest',
-#        'should', 'tried', 'Kaw', 'when', 'as', 'Nam', '2011', 'taking',
-#        'place', 'sent', 'Zaw', 'Tun', 'over', 'Namkham', 'Tin', 'health',
-#        'outside', 'I', 'While', 'Moe', 'Lay', 'prisons', 'Three',
-#        'further', 'reported', '(First', 'according', "don't", 'If', 'rice',
-#        'and'],
-#       dtype='|S78')
+key_array = numpy.array(word_to_count.keys())
+sort_idx = numpy.argsort(word_to_count.values())
+
+# Top n entries:
+n = 100
+
+top_words = key_array[sort_idx[-n:]][::-1]
+n_top_words = numpy.array(word_to_count.values())[sort_idx[-n:]][::-1]
+
+results_file = file("../data/word_counts_website.csv","w")
+[results_file.write("%s,%s\n"%(top_words[i],n_top_words[i])) for i in range(n)]
+results_file.close()
